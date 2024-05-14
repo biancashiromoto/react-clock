136f1a0b8004b285917197a4bf504d8c3b13e9aa
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-14 19:11:29 -0300

    feat: Implement method isDarkModeOn in class Utils

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -14,4 +14,12 @@ export default class Utils {
       weekDay: weekDay
     };
   }
+
+  /**
+   * Verifies if the dark color scheme is activated in user's system settings.
+   * @returns a boolean indicating wether the dark color scheme is activated.
+   */
+  public isDarkModeOn(): boolean {
+    return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
 } 
