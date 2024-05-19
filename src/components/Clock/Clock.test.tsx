0cf5fb7109d055de1d39611d4eecccf2b3f35998
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-19 18:52:42 -0300

    test: Update mode toggling tests

--- src/components/Clock/Clock.test.tsx
+++ src/components/Clock/Clock.test.tsx
@@ -2,46 +2,49 @@ import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
 import Clock from "./Clock";
 import App from "../../App";
+import { vi } from "vitest";
+import Utils from "../../utils/Utils";
+
+let matchMediaMock;
+const utils = new Utils();
 
 describe("Component 'App': ", () => {
+  beforeEach(() => render(<App />));
+
   it("Should contain a 'Clock' component", () => {
-    render(<App />);
     const clockComponent = screen.getByTestId("clock__container");
     expect(clockComponent).toBeInTheDocument();
   });
-});
-
-describe("Component 'Clock': ", () => {
-  beforeEach(() => {
-    render(<Clock />);
-  });
-
+  
   it("Should render correctly", async () => {  
     const clockContainer = screen.getByTestId("clock__container");
     expect(clockContainer).toBeInTheDocument();
 
     const dateTimeContainer = screen.getByTestId("date-time__container");
     expect(dateTimeContainer).toBeInTheDocument();
-
+    
     const h1Title = screen.getByTestId("formatted-time");
     expect(h1Title).toBeInTheDocument();
-
+    
     const paragraph = screen.getByTestId("formatted-weekday");
     expect(paragraph).toBeInTheDocument();
-
+    
     const toggleModeButton = screen.getByTestId("toggle-mode__button");
     expect(toggleModeButton).toBeInTheDocument();
   });
 
-  it("Should toggle between dark and light mode when 'toggle-mode__button' is clicked", () => {
-    const clockContainer = screen.getByTestId("clock__container");
+  it("Should toggle between dark and light modes when toggle mode button is clicked", () => {
+    matchMediaMock = vi.spyOn(Utils.prototype, 'isDarkModeOn');
 
-    expect(clockContainer).not.toHaveClass("light-mode__display");
+    const clockContainer = screen.getByTestId("clock__container");
     expect(clockContainer).toHaveClass("dark-mode__display");
-    
+    expect(clockContainer).not.toHaveClass("light-mode__display");
+
     const toggleModeButton = screen.getByTestId("toggle-mode__button");
     fireEvent.click(toggleModeButton);
-    
+
+    expect(matchMediaMock).toHaveBeenCalledTimes(1);
+
     expect(clockContainer).not.toHaveClass("dark-mode__display");
     expect(clockContainer).toHaveClass("light-mode__display");
   });
