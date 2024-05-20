26af1b5e299841b7dc6d1ae70f45cf4d85fafaf5
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-20 17:51:31 -0300

    test: Implement unit tests to component Clock

--- src/components/Clock/Clock.test.tsx
+++ src/components/Clock/Clock.test.tsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { fireEvent, render, screen } from "@testing-library/react";
 import App from "../../App";
-import { vi } from "vitest";
-import Utils from "../../utils/Utils";
-
-let matchMediaMock;
 
 describe("Component 'App': ", () => {
   beforeEach(() => render(<App />));
@@ -32,18 +28,26 @@ describe("Component 'App': ", () => {
   });
 
   it("Should toggle between dark and light modes when toggle mode button is clicked", () => {
-    matchMediaMock = vi.spyOn(Utils.prototype, 'isDarkModeOn');
+   const clockContainer = screen.getByTestId("clock__container");
+   expect(clockContainer).not.toHaveClass("dark-mode__display");
+   expect(clockContainer).toHaveClass("light-mode__display");
+   
+   const toggleModeButton = screen.getByTestId("toggle-mode__button");
+   fireEvent.click(toggleModeButton);
 
+   expect(clockContainer).toHaveClass("dark-mode__display");
+   expect(clockContainer).not.toHaveClass("light-mode__display");
+  });
+
+  it("Should toggle between dark and light modes when toggle mode button is clicked", () => {
     const clockContainer = screen.getByTestId("clock__container");
-    expect(clockContainer).toHaveClass("dark-mode__display");
-    expect(clockContainer).not.toHaveClass("light-mode__display");
-
-    const toggleModeButton = screen.getByTestId("toggle-mode__button");
-    fireEvent.click(toggleModeButton);
-
-    expect(matchMediaMock).toHaveBeenCalledTimes(1);
-
     expect(clockContainer).not.toHaveClass("dark-mode__display");
     expect(clockContainer).toHaveClass("light-mode__display");
-  });
-})
+    
+    const toggleModeButton = screen.getByTestId("toggle-mode__button");
+    fireEvent.click(toggleModeButton);
+ 
+    expect(clockContainer).toHaveClass("dark-mode__display");
+    expect(clockContainer).not.toHaveClass("light-mode__display");
+   });
+});
