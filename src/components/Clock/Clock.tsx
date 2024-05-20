b371350dfa73e29b6e57fa374797142f84fe0b9f
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2024-05-20 17:49:54 -0300

    refactor: Implement useEffect to manage time state change and add comments

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -4,20 +4,38 @@ import React from "react";
 import Utils from "../../utils/Utils";
 import "./Clock.css";
 
+/**
+ * 
+ * Clock component.
+ * Displays a digital clock and current date, allowing the user to toggle between dark and light modes.
+ */
 function Clock() {
+  /**
+   * Instance of the utilities class for time formatting and dark mode checking.
+   */
   const utils = new Utils();
 
-  const [time, setTime] = useState(new Date());
+  /**
+   * State to store the current date and time.
+   */
+  const [dateAndTime, setDateAndTime] = useState(new Date());
+
+  /**
+   * State to store the current mode (dark or light).
+   */
   const [isDarkModeOn, setIsDarkModeOn] = useState<boolean>(utils.isDarkModeOn());
 
+  /**
+   * Updates the time every second.
+   */
   useEffect(() => {
     const timer = setInterval(() => {
-      setTime(new Date());
+      setDateAndTime(new Date());
     }, 1000);
     return () => clearInterval(timer);
   }, []);
 
-  const { formattedTime, formattedDate } = utils.formatTimeAndDate(time);
+  const { formattedTime, formattedDate } = utils.formatDateAndTime(dateAndTime);
 
   return (
     <main
@@ -28,12 +46,14 @@ function Clock() {
         className="date-time__container"
         data-testid="date-time__container"
       >
+        {/* Displays the formatted time */}
         <h1
           aria-live="assertive"
           data-testid="formatted-time"
         >
           {formattedTime}
         </h1>
+        {/* Displays the formatted date */}
         <p
           aria-live="assertive"
           data-testid="formatted-weekday"
